608affb6b235a61e77d137c27fccbd777cb32d5c
Eremel <richardbailey@outlook.com>
2024-10-29 13:40:22 +0000

    loc txt fix

--- localization/default.lua
+++ localization/default.lua
@@ -9,6 +9,32 @@ return {
 			b_change = 'Changes',
 			malverk_low = 'LOWEST',
 			malverk_high = 'HIGHEST',
-        }
+			-- ['b_Cards'] = 'Playing Cards',
+			-- ['b_Joker'] = 'Jokers',
+			-- ['b_Tarot'] = 'Tarot Cards',
+			-- ['b_Planet'] = 'Planet Cards',
+			-- ['b_Spectral'] = 'Spectral Cards',
+			-- ['b_Enhanced'] = 'Enhancements',
+			-- ['b_Blind'] = 'Blinds',
+			-- ['b_Tag'] = 'Tags',
+			-- ['b_Back'] = 'Decks',
+			-- ['b_Seal'] = 'Seals',
+			-- ['b_Voucher'] = 'Vouchers',
+			-- ['b_Booster'] = 'Booster Packs',
+			k_space_station = 'Space Station',
+			k_realm = 'Realm',
+			k_universe = 'Universe',
+			k_plane = 'Plane',
+        },
     },
+	descriptions = {
+		texture_packs = {
+			default = {
+				name = 'Default',
+				text = {
+					'Base game textures'
+				}
+			}
+		}
+	}
 }
